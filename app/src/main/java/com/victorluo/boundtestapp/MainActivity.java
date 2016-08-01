3167a4e79048b0a8694b5de84b05d7bfae4e56be
Ryan Sparks <ryan@ryan-sparks.com>
2016-08-01 02:20:30 -0400

    Redesigned main page
    Redesigned the main page with only one button and the logo in the upper
    right.  Pressing the logo inflates a menu.

--- app/src/main/java/com/victorluo/boundtestapp/MainActivity.java
+++ app/src/main/java/com/victorluo/boundtestapp/MainActivity.java
@@ -2,46 +2,53 @@ package com.victorluo.boundtestapp;
 
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView boundlogo;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
 
+        boundlogo = (ImageView) findViewById(R.id.boundlogo);
 
-        final Button Meeting = (Button) findViewById(R.id.button3);
-        Meeting.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, FriendsActivity.class);
-                MainActivity.this.startActivity(i);
-            }
+        boundlogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, boundlogo);
+                popup.getMenuInflater().inflate(R.menu.settings_menu, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch(item.getItemId()) {
+                            case R.id.settings:
 
-        });
-
-
-        final Button CurrentMeeting = (Button) findViewById(R.id.button5);
-        CurrentMeeting.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, CurrentMeetupsActivity.class);
-                MainActivity.this.startActivity(i);
-
-
-            }
-
-        });
-
-        final Button settings = (Button) findViewById(R.id.button16);
-        settings.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, SettingActivity.class);
-                MainActivity.this.startActivity(i);
+                                break;
+                            case R.id.logout:
+                                mAuth.signOut();
+                                MainActivity.this.finish();
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                popup.show();
             }
         });
     }
